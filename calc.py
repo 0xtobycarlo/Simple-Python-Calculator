ee8dbfbe06c24d6eb77324a934e2530e25547a02
Toby Carlo <tobycarlo.c@gmail.com>
2022-03-22 10:19:10 +0000

    logic

--- calc.py
+++ calc.py
@@ -23,3 +23,14 @@ def multiply(x, y):
 
 def divide(x, y):
     return x / y
+
+
+print("Select Operation")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+while True:
+    # Take input from the user
+    choice = input("Enter Choice (1/2/3/4): ")
