1affd842f57e5b09fc131fe2b213b7373407f31e
Toby Carlo <tobycarlo.c@gmail.com>
2022-03-22 10:16:53 +0000

    expressions

--- calc.py
+++ calc.py
@@ -1,5 +1,25 @@
 # Program makes a simple calculator
 
 # This function adds two numbers
+
+
 def add(x, y):
     return x + y
+
+# This function subtracts two numbers
+
+
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+
+
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+
+
+def divide(x, y):
+    return x / y
