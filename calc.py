4fb202571dbb4b71da80535652015592bc4b0ffb
Toby Carlo <tobycarlo.c@gmail.com>
2022-03-22 10:25:16 +0000

    basic calc

--- calc.py
+++ calc.py
@@ -34,3 +34,23 @@ print("4. Divide")
 while True:
     # Take input from the user
     choice = input("Enter Choice (1/2/3/4): ")
+
+# Check if choice is one of the four options
+if choice in ('1', '2', '3', '4'):
+    num1 = float(input("Enter First Number: "))
+    num2 = float(input("Enter Second Number: "))
+
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+    break
+else:
+    print("Invalid Input")
